f22cb0c5b9375080ac8a0762aed355f14824e210
Nourhan Mohammed <84802524+N0URHAN01@users.noreply.github.com>
2023-10-25 17:41:08 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TodoWrapper from './comp/todowarapper';
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp/todowarapper.js
@@ -0,0 +1,81 @@
+import React , {useState } from "react";
+import Todoform from "./todoform";
+import Todo from "./todo";
+
+import EditTodoForm from "./edittodoform";
+
+import { v4 as uuidv4 } from "uuid";
+uuidv4 ();
+
+
+const TodoWrapper = () => {
+
+  const [todos, setTodos] = useState([])
+
+ 
+  const addTodo = todo => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false }
+    ]);
+    console.log(todos)
+
+  }
+
+  const toggleComplete =(id)=>{
+    setTodos(
+      todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    )
+  );
+  }
+  const deleteTodo = (id) => {
+    setTodos(
+      todos.filter((todo) => todo.id !== id)
+    );
+  };
+  
+  const editTodo =(id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  }
+const editTask = (task, id) => {
+  setTodos(
+    todos.map((todo) =>
+      todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+    )
+  );
+};
+
+
+  
+  return (
+    <div className="TodoWrapper">
+
+      <h1> Get Things Done </h1>
+      < Todoform addTodo={addTodo} />
+      {todos.map ((todo , index) => (
+       todo.isEditing ?(
+        <EditTodoForm editTodo={editTask} task={todo} />
+
+        ):(
+          < Todo task={todo}  key={index}
+        toggleComplete={toggleComplete } 
+        deleteTodo={deleteTodo}
+        editTodo={editTodo}
+        />
+
+        )
+       
+        
+      )
+      )}
+
+    </div>
+  )
+}
+
+export default TodoWrapper ;
